81e5c21343e62f209bff209a6648efa411fcdb86
PramodBisht <pramodpsb@outlook.com>
2014-08-01 12:16:08 +0530

    code improvement

--- index.php
+++ index.php
@@ -11,21 +11,52 @@
 
 <?php
 if(isset($_GET['url'])&&!empty($_GET['url'])){
-include('library/simple_html_dom.php');
- 
+include('library/simple_html_dom.php');  //using the simple html dom for php library for web scrapping
+
 $url=$_GET['url'];
 
-$html = file_get_html($url);
+$html = file_get_html($url); //required for web scrapping 
 
 if(strpos($url,"linkedin")!==false){
-
 	//block for linkedin
-echo "Job title is ".$html->find('p.headline-title')[0];
-echo "Connection are ".$html->find('dd.overview-connections')[0];
-echo "My summary is<br>";
-echo $html->find('p.description')[0];
-echo "My skills sets are<br>";
-echo $html->find('ol.skills')[0];
+
+	try{
+		if(isset($html->find('p.headline-title')[0])==false){
+			throw new Exception("job title is hidden"."<br>");
+		}
+		else{
+			echo $html->find('p.headline-title')[0]."<br>";
+		}
+	}
+	catch(Exception $e){
+		echo $e->getMessage();
+	}
+	try{
+		if(isset($html->find('dd.overview-connections')[0])==false){
+			throw new Exception("no of connections are hidden"."<br>");
+		}
+		else{
+			echo $html->find('dd.overview-connections')[0]."<br>";
+		}
+	}
+	catch(Exception $e){
+		echo $e->getMessage();
+	}
+
+	try{
+		if(isset($html->find('p.description')[0])==false){
+			throw new Exception("no of connections are hidden"."<br>");
+		}
+		else{
+			echo "My skills sets are<br>";;
+			echo $html->find('ol.skills')[0]."<br>";
+		}
+	}
+	catch(Exception $e){
+		echo $e->getMessage();
+	}
+	
+	
 }else
 if(strpos($url,"twitter")!==false)
 {
@@ -73,44 +104,52 @@ if(strpos($url,"twitter")!==false)
 else
 if(strpos($url,"angel")>=0){
 	echo $url;
+	$msg="";
 	$name=$html->find("h1.name")[0];
+	echo $name;
+	$counter=0;
 	try{
 	$bio=$html->find('h2.bio')[0];
-	if(isset($bio)==false){
-		throw new Exception("User have not put bio");
-	}
+		if(isset($bio)==false){
+			$msg.="User have not put bio"."<br>";
+			echo $msg;
+			$counter++;
+			//throw new Exception("User have not put bio");
+		}else{
+			echo $bio."<br>";
+		}
+		echo "followers and following";
+		echo $html->find('div.follows')[0];
+		if(isset($html->find('div.item')[5])==false){
+			$msg.="User have not mention any skills"."<br>";
+			$counter++;
+		//throw new Exception("User have not mention any skills"."<br>");
+		}else{
+			echo "My skills are";
+			echo $html->find('div.item')[5]."<br>";
+		}
+		if(isset($html->find('div.item')[6])==false){
+			$msg.="User have not mention oppurtunity he/she is looking for"."<br>";
+			$counter++;
+		//throw new Exception("User have not mention oppurtunity he/she is looking for"."<br>");
+		}else{
+			echo $html->find('div.item')[6]."<br>";
+		}
+		if($counter!=0){
+			//echo "message is ".$msg;
+			throw new Exception($msg);
+			
+		}
 	}
 	catch(Exception $e){
+		//echo "inside catch";
 		echo $e->getMessage();
 	}
-	echo $name;
-	echo $bio;
-	echo "followers and following";
-	echo $html->find('div.follows')[0];
 	
-	try{
 	
-	if(isset($html->find('div.item')[5])==false){
-		throw new Exception("User have not mention any skills"."<br>");
-	}else{
-		echo "My skills are";
-		echo $html->find('div.item')[5]."<br>";
-	}
-	}
-	catch(Exception $e){
-		echo $e->getMessage();
-	}
-	try{
-	if(isset($html->find('div.item')[6])==false){
-		throw new Exception("User have not mention oppurtunity he/she is looking for"."<br>");
-	}else{
-		
-		echo $html->find('div.item')[6]."<br>";
-	}
-	}
-	catch(Exception $e){
-		echo $e->getMessage();
-	}
+	
+	
+	
 	}
 
 }
