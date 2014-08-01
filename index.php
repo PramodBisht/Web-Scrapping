86e488f4dbe3155e7d25a658d267b48204154228
PramodBisht <pramodpsb@outlook.com>
2014-08-01 20:06:47 +0530

    current code is getting the changes and putting them into db

--- index.php
+++ index.php
@@ -21,46 +21,139 @@ if(strpos($url,"linkedin")!==false){
 	//block for linkedin
 	$msg="";
 	$counter=0;
-	try{
+	$skill="";
+	$designation="";$no_of_connection="";
+	$description=array();
+	$description2=array();
+	//$skills=array();
+	//$skills2=array();
+	
+		
+		$name=$html->find('span.full-name')[0]->plaintext;
+		echo $name."<br>";
 		if(isset($html->find('p.headline-title')[0])==false){
-			$msg.="job title is hidden"."<br>";
-			$counter++;
+			$designation="job title is hidden"."<br>";
 		}
 		else{
-			echo $html->find('p.headline-title')[0]->plaintext."<br>";
+			$designation=$html->find('p.headline-title')[0]->plaintext;
+			echo $designation."<br>";
 		}
 		if(isset($html->find('dd.overview-connections')[0])==false){
-			$msg.="no of connections are hidden"."<br>";
-			$counter++;
+			$no_of_connection="0";
 		}
 		else{
-			echo $html->find('dd.overview-connections')[0]."<br>";
+			$no_of_connection=$html->find('dd.overview-connections')[0]->plaintext;
+			$no_of_connection=substr($no_of_connection, 0,strpos($no_of_connection, "+"));
+			echo $no_of_connection."<br>";
 		}
 		if(isset($html->find('p.description')[0])==false){
-			$msg.="no of connections are hidden"."<br>";
-			$counter++;
+			$desc="description is hidden";
 		}
 		else{
-			echo $html->find('p.description')[0]->plaintext."<br>";
+			$desc=$html->find('p.description')[0]->plaintext;
+			str_replace("&#x25ba;",",", $desc);
+			echo $desc;
 		}
 		if(isset($html->find('ol.skills')[0])==false){
-			$msg.="skill are not public";
-			$counter++;
-
+			$skill="skill are not public";
+			
 		}
 		else{
 			echo "My skills sets are<br>";;
-			echo $html->find('ol.skills')[0]->plaintext."<br>";
+			$skill=$html->find('ol.skills')[0]->plaintext."<br>";
+			echo $skill;
 		}
-		if($counter!=0){
-			throw new Exception($msg);
-		}
+		
+	
+	 	
+	$db=mysqli_connect("localhost","root",NULL,"data");
+	if (mysqli_connect_errno()) {
+	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	}
-	catch(Exception $e){
-		echo $e->getMessage();
+
+	$query = "SELECT * FROM `linkedin` WHERE `link`=\"$url\";";
+	
+	$result=mysqli_query($db,$query);
+	if(!$result){
+		   printf("Error: %s\n", mysqli_error($db));
+    exit();
+	}
+	while($row = mysqli_fetch_array($result)) {
+	  echo $row['link'];
+	  echo "<br>";	
+	  echo $row['Name'];
+	  echo "<br>";
+	  echo $row['designation'];
+	  echo "<br>";
+	  echo $row['connection'];
+	  echo "<br>";
+	  echo $row['description'];
+	  echo "<br>";
+	  $counter=0;$counter1=0;$counter2=0;$counter3=0;$counter4=0;
+	  $new=$row['Name'];
+	  $name=trim($name);
+	  $name=str_replace("  ", " ", $name);
+	  $designation=trim($designation);
+	  $designation=str_replace("  ", " ", $designation);
+	  $no_of_connection=trim($no_of_connection);
+	  $no_of_connection=str_replace("  ", " ", $no_of_connection);
+	  $desc=trim($desc);
+	  $desc=str_replace("  ", " ", $desc);
+	  if(strcmp($name,$row['Name'])!==0){
+         $counter++;
+	  }
+	  if(strcmp($row['designation'],$designation)!==0){
+	  	 $counter1++;
+	  }
+	  if(strcmp($row['connection'],$no_of_connection)!==0){
+	  	
+	  	 $counter2++;
+	  }
+	  if(strcmp($row['description'],$desc)!==0){
+	  	 $counter3++;
+	  }
+	  //echo gettype
+	  if(strcmp($row['skills'],$skill)!==0){
+	  	 $counter4++;
+	  }
+
+
+	  if($counter!=0){
+	  	$msg.= "name is changed<br>";
+	  	$q="UPDATE `linkedin` SET `Name`=\"$name\" WHERE `link`=\"$url\";";
+	  	mysqli_query($db,$q);
+
+	  }
+	  if($counter1!=0){
+	  	$msg.= "designation is changed<br>";
+	  	$q="UPDATE `linkedin` SET `designation`=\"$designation\" WHERE `link`=\"$url\";";
+	  	mysqli_query($db,$q);
+	  }
+	  if($counter2!=0){
+	  	$msg.= "no of connection are changed<br>";
+	  	$q="UPDATE `linkedin` SET `connection`=\"$no_of_connection\" WHERE `link`=\"$url\";";
+	  	mysqli_query($db,$q);
+	  }
+	  if($counter3!=0){
+	  	$msg.= "summary is changed<br>";
+	  	$q="UPDATE `linkedin` SET `description`=\"$desc\" WHERE `link`=\"$url\";";
+	  	mysqli_query($db,$q);
+	  }
+	  if($counter4!=0){
+	  	$msg.= "skills are changed<br>";
+	  	$q="UPDATE `linkedin` SET `skills`=\"$skill\" WHERE `link`=\"$url\";";
+	  	mysqli_query($db,$q);
+	  }
+
 	}
 	
 	
+
+
+
+mysqli_close($db);
+
+
 }else
 if(strpos($url,"twitter")!==false)
 {
@@ -109,7 +202,7 @@ if(strpos($url,"angel")>=0){
 	echo $name;
 	$counter=0;
 	try{
-	$bio=$html->find('h2.bio')[0];
+		$bio=$html->find('h2.bio')[0];
 		if(isset($bio)==false){
 			$msg.="User have not put bio"."<br>";
 			//echo $msg;
